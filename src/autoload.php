88334fb881e42c9bc10354f22fbf76897600918e
Paul Waring <paul.waring@manchester.ac.uk>
2015-01-22 13:31:30 +0000

    Comment autoload function

--- src/autoload.php
+++ src/autoload.php
@@ -1,17 +1,21 @@
 <?php
 
 spl_autoload_register(function($class) {
+  // Assume all classes are under this single namespace
   $prefix = 'UoMCS\\OpenBadges\\Backend\\';
 
   $base_dir = __DIR__ . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR;
 
   $len = strlen($prefix);
 
+  // Skip this autoloader if class name does not begin with prefix
   if (strncmp($prefix, $class, $len) !== 0)
   {
     return;
   }
 
+  // Relative class is the last part of full class name
+  // e.g. UoMCS\OpenBadges\Backend\Utility => Utility
   $relative_class = substr($class, $len);
 
   $file = $base_dir . str_replace('\\', DIRECTORY_SEPARATOR, $relative_class) . '.php';
