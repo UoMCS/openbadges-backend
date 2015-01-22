ad55844682c33eea9265aa77d15405c03d4151c8
Paul Waring <paul.waring@manchester.ac.uk>
2015-01-22 15:49:59 +0000

    Badges schema

--- data/sqlite.sql
+++ data/sqlite.sql
@@ -1,8 +1,20 @@
-CREATE TABLE issuers (
+CREATE TABLE issuers
+(
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
   url TEXT NOT NULL,
   description TEXT NOT NULL,
   image TEXT,
   email TEXT
+);
+
+CREATE TABLE badges
+(
+  id INTEGER PRIMARY KEY,
+  issuer_id INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL,
+  image TEXT NOT NULL,
+  criteria TEXT NOT NULL,
+  FOREIGN KEY (issuer_id) REFERENCES issuers(id)
 );
