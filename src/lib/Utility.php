cf66f850ca3aa057d30e4b3ea4c8b6fc4602067f
Paul Waring <paul.waring@manchester.ac.uk>
2015-01-22 13:32:00 +0000

    Working identityHash implementation
    Unit test now passes.

--- src/lib/Utility.php
+++ src/lib/Utility.php
@@ -4,8 +4,14 @@ namespace UoMCS\OpenBadges\Backend;
 
 class Utility
 {
+  const HASH_ALGORITHM = 'sha512';
+
   public static function identityHash($str, $salt)
   {
-    return '';
+    $str .= $salt;
+    $hash = hash(self::HASH_ALGORITHM, $str);
+    $hash = self::HASH_ALGORITHM . '$' . $hash;
+
+    return $hash;
   }
 }
