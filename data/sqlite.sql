bcb7ede39652d9b7d49f373a28ad624cd1202138
Paul Waring <paul.waring@manchester.ac.uk>
2015-01-22 16:12:32 +0000

    Earners and Earned Badges schemas

--- data/sqlite.sql
+++ data/sqlite.sql
@@ -18,3 +18,25 @@ CREATE TABLE available_badges
   criteria TEXT NOT NULL,
   FOREIGN KEY (issuer_id) REFERENCES issuers(id)
 );
+
+CREATE TABLE earners
+(
+  id INTEGER PRIMARY KEY,
+  hash TEXT NOT NULL UNIQUE,
+  type TEXT NOT NULL
+);
+
+CREATE TABLE earned_badges
+(
+  id INTEGER PRIMARY KEY,
+  earner_id INTEGER NOT NULL,
+  badge_id INTEGER NOT NULL,
+  verification_type TEXT NOT NULL,
+  verification_url TEXT NOT NULL,
+  issued TEXT NOT NULL,
+  image TEXT,
+  evidence TEXT,
+  expires TEXT,
+  FOREIGN KEY (earner_id) REFERENCES earners(id),
+  FOREIGN KEY (badge_id) REFERENCES available_badges(id)
+);
