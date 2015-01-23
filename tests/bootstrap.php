6f40e7d8a06e789c67e23ddb7ecb10530d56a6cf
Paul Waring <paul.waring@manchester.ac.uk>
2015-01-23 14:41:18 +0000

    Prefix all web server variables with httpd_
    This allows us to start other server options (e.g. Selenium) at a later date.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../src/autoload.php';
 
 date_default_timezone_set('Europe/London');
 
-$command = sprintf(
+$httpd_command = sprintf(
   'php -S %s:%d -t %s >/dev/null 2>&1 & echo $!',
   WEB_SERVER_HOST,
   WEB_SERVER_PORT,
@@ -12,25 +12,25 @@ $command = sprintf(
 );
 
 // Execute command and store process ID
-$output = array();
-exec($command, $output);
-$pid = (int) $output[0];
+$httpd_output = array();
+exec($httpd_command, $httpd_output);
+$httpd_pid = (int) $httpd_output[0];
 
 echo sprintf(
   '%s - Web server started on %s:%d with PID %d',
   date('r'),
   WEB_SERVER_HOST,
   WEB_SERVER_PORT,
-  $pid
+  $httpd_pid
 ) . PHP_EOL;
 
 // Kill web server when process ends
-register_shutdown_function(function() use ($pid) {
+register_shutdown_function(function() use ($httpd_pid) {
   echo sprintf(
     '%s - Killing process with ID %d',
     date('r'),
-    $pid
+    $httpd_pid
   ) . PHP_EOL;
 
-  exec('kill ' . $pid);
+  exec('kill ' . $httpd_pid);
 });
