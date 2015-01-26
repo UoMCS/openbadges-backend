ef285b77cb5eed3bbef286d3e5409b0dc49326f4
Paul Waring <paul.waring@manchester.ac.uk>
2015-01-26 13:44:37 +0000

    Test for /issuers which passes

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -4,6 +4,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->get('/issuers', function() use($app) {
+  $body = array();
+  $json_body = json_encode($body);
+
+  return $json_body;
+});
+
 $app->get('/issuers/{id}', function($id) use ($app) {
 
 });
--- tests/IssuerTest.php
+++ tests/IssuerTest.php
@@ -9,4 +9,20 @@ class IssuerTest extends \PHPUnit_Framework_TestCase
     $issuer = new Issuer();
     $this->assertInstanceOf('UoMCS\OpenBadges\Backend\Issuer', $issuer);
   }
+
+  public function testIssuersUrl()
+  {
+    $url = WEB_SERVER_BASE_URL . '/issuers';
+
+    $client = new \Zend\Http\Client();
+    $client->setUri($url);
+    $response = $client->send();
+
+    $this->assertTrue($response->isOk(), 'Accessing /issuers did not return 2xx code');
+
+    $body = $response->getBody();
+    $json_body = json_decode($body, true);
+
+    $this->assertNotNull($json_body, 'Body is not valid JSON');
+  }
 }
