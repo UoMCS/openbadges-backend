637b2f9216c03c9fa0b6970cd1eb8f9711123b46
Paul Waring <paul.waring@manchester.ac.uk>
2015-01-26 13:27:31 +0000

    Test /badges endpoint

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -5,11 +5,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 
 $app->get('/issuers/{id}', function($id) use ($app) {
-  
+
 });
 
 $app->get('/badges', function() use ($app) {
-  return 'List of all badges';
+
 });
 
 $app->run();
--- tests/BadgeTest.php
+++ tests/BadgeTest.php
@@ -9,4 +9,20 @@ class BadgeTest extends \PHPUnit_Framework_TestCase
     $badge = new Badge();
     $this->assertInstanceOf('UoMCS\OpenBadges\Backend\Badge', $badge);
   }
+
+  public function testBadgesUrl()
+  {
+    $url = WEB_SERVER_BASE_URL . '/badges';
+
+    $client = new \Zend\Http\Client();
+    $client->setUri($url);
+    $response = $client->send();
+
+    $this->assertTrue($response->isOk(), 'Accessing /badges did not return 2xx code');
+
+    $body = $response->getBody();
+    $json_body = json_decode($body, true);
+
+    $this->assertNotNull($json_body, 'Body is not valid JSON');
+  }
 }
