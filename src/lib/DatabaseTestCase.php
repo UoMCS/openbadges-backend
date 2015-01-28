1e4130699042675737e06798e246902eb5a3fec7
Paul Waring <paul.waring@manchester.ac.uk>
2015-01-28 16:40:13 +0000

    Abstract class for testing classes which require database access

---
+++ src/lib/DatabaseTestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace UoMCS\OpenBadges\Backend;
+
+abstract class DatabaseTestCase extends \PHPUnit_Extensions_Database_TestCase
+{
+  static private $pdo = null;
+  private $connection = null;
+
+  final protected function getConnection()
+  {
+    if ($this->connection === null)
+    {
+      if (self::$pdo === null)
+      {
+        self::$pdo = SQLite::getInstance();
+      }
+
+      $this->connection = $this->createDefaultDBConnection(self::$pdo);
+    }
+
+    return $this->connection;
+  }
+
+  protected function getDataSet()
+  {
+    return new \PHPUnit_Extensions_Database_DataSet_YamlDataSet(__DIR__ . '/../../data/test.yml');
+  }
+}
