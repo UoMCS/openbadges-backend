ce6c03b37474edb0ff8c1d6dd05c85d0e89ded21
Paul Waring <paul.waring@manchester.ac.uk>
2015-01-30 11:33:00 +0000

    Set fetch mode to associative array by default

--- src/lib/SQLite.php
+++ src/lib/SQLite.php
@@ -12,7 +12,7 @@ class SQLite
     {
       self::$instance = new \PDO(OPEN_BADGES_DB_DSN);
       self::$instance->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-      self::$instance->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+      self::$instance->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
     }
 
     return self::$instance;
