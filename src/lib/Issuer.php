21c61d96583e2c15b381a0f7ac26ea52581e3182
Paul Waring <paul.waring@manchester.ac.uk>
2015-01-30 12:51:23 +0000

    Select all columns in Issuer::get

--- src/lib/Issuer.php
+++ src/lib/Issuer.php
@@ -12,12 +12,12 @@ class Issuer extends Base
     'image' => null,
     'email' => null,
   );
-  
+
   public static function get($id)
   {
     $db = SQLite::getInstance();
 
-    $sql = 'SELECT id, name, url, description, image, email FROM issuers WHERE id = :id';
+    $sql = 'SELECT * FROM issuers WHERE id = :id';
     $sth = $db->prepare($sql);
     $sth->bindParam(':id', $id, \PDO::PARAM_INT);
     $sth->execute();
