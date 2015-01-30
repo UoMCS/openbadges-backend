94b1f31a458c1c54e2ed29646de0e91fa5ed0e99
Paul Waring <paul.waring@manchester.ac.uk>
2015-01-30 12:32:23 +0000

    Data elements for earned badges

--- src/lib/EarnedBadge.php
+++ src/lib/EarnedBadge.php
@@ -4,5 +4,16 @@ namespace UoMCS\OpenBadges\Backend;
 
 class EarnedBadge extends Base
 {
-  
+  public $data = array(
+    'uid' => null,
+    'earner_id' => null,
+    'badge_id' => null,
+    'verification_type' => null,
+    'verification_url' => null,
+    'issued' => null,
+    'image' => null,
+    'evidence' => null,
+    'expires' => null,
+    'revoked' => null,
+  );
 }
